0a57079b7dd7f001623050ef339d8af57155e7dc
Alexander Malyshev <lex.malyshev@gmail.com>
2011-06-02 11:11:08 -0400

    Moved method locations in queue.c to match those in queue.h

--- queue/queue.c
+++ queue/queue.c
@@ -2,24 +2,23 @@
 #include "queue.h"
 #include "queue-int.h"
 
-/* queue_clear - removes all the items from the given queue and frees all
-                 corresponding nodes
- * Fails: q is NULL */
-int queue_clear(queue *q) {
-    qnode *node, *deadnode;
-
+/* queue_init - allocs and initializes a new empty queue at *q
+ * Fails: q is NULL, or calloc fails */
+int queue_init(queue **q) {
     if (q == NULL)
         return 1;
+    if ((*q = calloc(1, sizeof(queue))) == NULL)
+        return 1;
+    return 0;
+}
 
-    node = q->front;
-    while (node != NULL) {
-        deadnode = node;
-        node = node->next;
-        free(deadnode);
-    }
-    q->front = NULL;
-    q->back = NULL;
-    q->len = 0;
+/* queue_destroy - clears out the queue of all items, frees all nodes as well
+                   as the queue struct itself
+ * Fails: q is NULL */
+int queue_destroy(queue *q) {
+    if (queue_clear(q))
+        return 1;
+    free(q);
     return 0;
 }
 
@@ -43,16 +42,6 @@ void *queue_dequeue(queue *q) {
     return item;
 }
 
-/* queue_destroy - clears out the queue of all items, frees all nodes as well
-                   as the queue struct itself
- * Fails: q is NULL */
-int queue_destroy(queue *q) {
-    if (queue_clear(q))
-        return 1;
-    free(q);
-    return 0;
-}
-
 /* queue_enqueue - adds the given item to the back of the given queue
  * Fails: q is NULL or item is NULL, or if malloc fails in init_node */
 int queue_enqueue(queue *q, void *item) {
@@ -72,16 +61,6 @@ int queue_enqueue(queue *q, void *item) {
     return 0;
 }
 
-/* queue_init - allocs and initializes a new empty queue at *q
- * Fails: q is NULL, or calloc fails */
-int queue_init(queue **q) {
-    if (q == NULL)
-        return 1;
-    if ((*q = calloc(1, sizeof(queue))) == NULL)
-        return 1;
-    return 0;
-}
-
 /* queue_peek - returns the first item in the given queue if it exists,
                 NULL if the queue is empty
  * Fails: q is NULL or q is empty */
@@ -89,6 +68,35 @@ void *queue_peek(queue *q) {
     if (q == NULL || q->front == NULL)
         return NULL;
     return q->front->data;
+}
+
+/* queue_clear - removes all the items from the given queue and frees all
+                 corresponding nodes
+ * Fails: q is NULL */
+int queue_clear(queue *q) {
+    qnode *node, *deadnode;
+
+    if (q == NULL)
+        return 1;
+
+    node = q->front;
+    while (node != NULL) {
+        deadnode = node;
+        node = node->next;
+        free(deadnode);
+    }
+    q->front = NULL;
+    q->back = NULL;
+    q->len = 0;
+    return 0;
+}
+
+/* queue_len - returns the length of the given queue
+ * Fails: q is NULL */
+long queue_len(queue *q) {
+    if (q == NULL)
+        return -1;
+    return q->len;
 }
 
 /* init_node - allocs a new qnode with the given item as the data
@@ -99,12 +107,4 @@ static int init_node(qnode **node, void *item) {
         return 1;
     (*node)->data = item;
     return 0;
-}
-
-/* queue_len - returns the length of the given queue
- * Fails: q is NULL */
-long queue_len(queue *q) {
-    if (q == NULL)
-        return -1;
-    return q->len;
 }
