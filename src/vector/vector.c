622dd9d65726cdb87138c1fa0c208baa02443fbd
Alexander Malyshev <lex.malyshev@gmail.com>
2011-12-28 17:39:02 -0500

    Two new vector methods and added doxygen to header
    Fixed bad realloc, bad resizing, bad everything.

--- src/vector/vector.c
+++ src/vector/vector.c
@@ -5,6 +5,7 @@
 
 static size_t find_last(vector *vec, size_t start);
 static void resize(vector *vec, size_t cap);
+static void swap(vector *vec, size_t i, size_t j);
 
 void vector_init(vector *vec, size_t cap) {
     assert(vec != NULL);
@@ -43,13 +44,43 @@ void vector_clear(vector *vec) {
     memset(vec->elems, 0, vec->last);
 }
 
+void vector_compact(vector *vec) {
+    size_t empty;
+    size_t i;
+
+    assert(vec != NULL);
+
+    empty = vec->cap;
+    for (i = 0; i <= vec->last; ++i) {
+        /* if slot is empty we might need to initialize empty */
+        if (vec->elems[i] == NULL) {
+            if (empty == vec->cap)
+                empty = i;
+        } else if (empty != vec->cap) {
+            /* move element into our last empty slot */
+            swap(vec, empty, i);
+            /* find new empty */
+            while (vec->elems[empty] != NULL) {
+                ++empty;
+                /* if no more compacting is needed */
+                if (empty == vec->last) {
+                    vec->last = empty - 1;
+                    return;
+                }
+            }
+        }
+    }
+
+    vec->last = (empty != 0 ? empty - 1 : 0);
+}
+
 void *vector_insert(vector *vec, size_t i, void *elem) {
     void *old;
 
     assert(vec != NULL);
     assert(elem != NULL);
 
-    /* if i is "unreasonably" big, just resize to i immediately, and
+    /* if i is really big, just resize to i + 1 immediately, and
      * if i is 0, then resize to the default capacity */
     if (i >= vec->cap) {
         if (i == 0)
@@ -57,7 +88,7 @@ void *vector_insert(vector *vec, size_t i, void *elem) {
         else if (i < VECTOR_GROW_CAP(vec->cap))
             resize(vec, VECTOR_GROW_CAP(vec->cap));
         else
-            resize(vec, i);
+            resize(vec, i + 1);
     }
 
     old = vec->elems[i];
@@ -81,7 +112,9 @@ void *vector_remove(vector *vec, size_t i) {
     void *old;
 
     assert(vec != NULL);
-    assert(i < vec->cap);
+
+    if (i >= vec->cap)
+        return NULL;
 
     old = vec->elems[i];
     vec->elems[i] = NULL;
@@ -99,9 +132,35 @@ void vector_resize(vector *vec, size_t cap) {
     resize(vec, cap);
 }
 
+void vector_swap(vector *vec, size_t i, size_t j) {
+    size_t max;
+
+    assert(vec != NULL);
+
+    /* handles vec->cap = 0, i = 0, j = 0 */
+    if (i == j)
+        return;
+
+    /* if both are out-of-bounds, then they might as well be swapped already */
+    if (i >= vec->cap && j >= vec->cap)
+        return;
+
+    max = (i < j ? j : i);
+
+    /* if max is really big, just resize to max + 1 directly */
+    if (max >= vec->cap) {
+        if (max < VECTOR_GROW_CAP(vec->cap))
+            resize(vec, VECTOR_GROW_CAP(vec->cap));
+        else
+            resize(vec, max + 1);
+    }
+
+    swap(vec, i, j);
+}
+
 void vector_trim(vector *vec) {
     assert(vec != NULL);
-    resize(vec, vec->last);
+    resize(vec, vec->last + 1);
 }
 
 static size_t find_last(vector *vec, size_t start) {
@@ -118,7 +177,7 @@ static size_t find_last(vector *vec, size_t start) {
 }
 
 static void resize(vector *vec, size_t cap) {
-    vec->elems = realloc(vec->elems, cap);
+    vec->elems = realloc(vec->elems, cap * sizeof(void *));
 
     /* everything after the old capacity needs to be zeroed out */
     if (cap > vec->cap)
@@ -129,4 +188,10 @@ static void resize(vector *vec, size_t cap) {
         vec->elems = NULL;
 
     vec->cap = cap;
+}
+
+static void swap(vector *vec, size_t i, size_t j) {
+    void *temp = vec->elems[i];
+    vec->elems[i] = vec->elems[j];
+    vec->elems[j] = temp;
 }
--- src/vector/vector.h
+++ src/vector/vector.h
@@ -1,25 +1,154 @@
+/** @file vector.h
+ *  @brief Header for a dynamic array data structure.
+ *
+ *  A <tt>vector</tt> is a dynamic array. Inserts and pushes into the
+ *  <tt>vector</tt> will always succeed provided there is enough memory on the
+ *  system. Elements are stored as generic pointers (<tt>void *</tt>), however
+ *  <tt>NULL</tt> cannot be stored. Getting a <tt>NULL</tt> back as an element
+ *  from the <tt>vector</tt> means that position in the <tt>vector</tt> was
+ *  empty.
+ *
+ *  @author Alexander Malyshev
+ */
+
 #ifndef VECTOR_H_
 #define VECTOR_H_
 
+#include <stddef.h>
+
+/** @brief The default capacity of a new <tt>vector</tt>. */
 #define VECTOR_DEFAULT_CAP 32
+
+/** @brief The default rate used when resizing. */
 #define VECTOR_GROW_CAP(cap) (3*(cap)/2)
 
+/** @brief A dynamic array. */
 typedef struct {
-    void **elems;
-    size_t cap;
-    size_t last;
+    void **elems; /**< the array of elements. */
+    size_t cap;   /**< total capacity of the array. */
+    size_t last;  /**< index of the last stored element. */
 } vector;
 
+/** @brief Initializes a new <tt>vector</tt>.
+ *
+ *  Asserts that <tt>vec</tt> is not NULL.
+ *  If <tt>cap</tt> is 0, then no allocations will occur.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ *  @param cap the desired capacity.
+ */
 void vector_init(vector *vec, size_t cap);
+
+/** @brief Frees the dynamic array in <tt>vec</tt>/
+ *
+ *  Asserts that <tt>vec</tt> is not <tt>NULL</tt>.
+ *  A vector that has been destroyed needs to be reinitialized if
+ *  it is to be used again.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ */
 void vector_destroy(vector *vec);
 
+/** @brief Returns the element at position <tt>i</tt>.
+ *
+ *  Asserts that <tt>vec</tt> is not <tt>NULL</tt>.
+ *  Trying to access an out-of-bounds element returns <tt>NULL</tt>.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ *  @param i the index.
+ *  @return The element at <tt>vec[i]</tt>.
+ */
 void *vector_at(vector *vec, size_t i);
+
+/** @brief Clears out <tt>vec</tt>.
+ *
+ *  Asserts that <tt>vec</tt> is not <tt>NULL</tt>.
+ *  Will not resize <tt>vec</tt>.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ */
 void vector_clear(vector *vec);
+
+/** @brief Compacts all the elements in <tt>vec</tt> to the left.
+ *
+ *  Asserts that <tt>vec</tt> is not <tt>NULL</tt>.
+ *  Will not resize <tt>vec</tt>.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ */
+void vector_compact(vector *vec);
+
+/** @brief Inserts <tt>elem</tt> at <tt>vec[i]</tt>.
+ *
+ *  Asserts that <tt>vec</tt> and <tt>elem</tt> are not <tt>NULL</tt>.
+ *  Will resize <tt>vec</tt> if <tt>i</tt> is out-of-bounds.
+ *  Does not shift over elements.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ *  @param i the index.
+ *  @param elem the element.
+ *  @return The old element at <tt>vec[i]</tt>.
+ */
 void *vector_insert(vector *vec, size_t i, void *elem);
+
+/** @brief Removes the last element from <tt>vec</tt>.
+ *
+ *  Asserts that <tt>vec</tt> is not <tt>NULL</tt>.
+ *  Equivalent to <tt>vector_remove(vec, vec->last)</tt>.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ *  @return The last element in <tt>vec</tt>.
+ */
 void *vector_pop(vector *vec);
+
+/** @brief Adds <tt>elem</tt> as the last element of <tt>vec</tt>.
+ *
+ *  Asserts that <tt>vec</tt> and <tt>elem</tt> are not <tt>NULL</tt>.
+ *  Equivalent to <tt>vector_insert(vec, vec->last + 1, elem)</tt>.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ *  @param elem the element.
+ */
 void vector_push(vector *vec, void *elem);
+
+/** @brief Removes <tt>vec[i]</tt>.
+ *
+ *  Asserts that <tt>vec</tt> is not <tt>NULL</tt>.
+ *  Will not resize <tt>vec</tt>.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ *  @param i the index.
+ *  @return The element at <tt>vec[i]</tt>.
+ */
 void *vector_remove(vector *vec, size_t i);
+
+/** @brief Resizes <tt>vec</tt> to capacity <tt>cap</tt>.
+ *
+ *  Asserts that <tt>vec</tt> is not <tt>NULL</tt>.
+ *  Can shrink or grow <tt>vec</tt>, and resizing to capacity 0 will free
+ *  its dynamic array.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ *  @param cap the desired capacity.
+ */
 void vector_resize(vector *vec, size_t cap);
+
+/** @brief Swaps <tt>vec[i]</tt> with <tt>vec[j]</tt>
+ *
+ *  Asserts that <tt>vec</tt> is not <tt>NULL</tt>.
+ *  Will resize <tt>vec</tt> if either <tt>i</tt> or <tt>j</tt> is
+ *  out-of-bounds, but not if both are.
+ *
+ */
+void vector_swap(vector *vec, size_t i, size_t j);
+
+/** @brief Shrinks <tt>vec</tt> as much as possible without losing elements.
+ *
+ *  Asserts that <tt>vec</tt> is not <tt>NULL</tt>.
+ *  Equivalent to <tt>vector_resize(vec, vec->last + 1)</tt>.
+ *
+ *  @param vec the address of the <tt>vector</tt>.
+ */
 void vector_trim(vector *vec);
 
 #endif /* VECTOR_H_ */
