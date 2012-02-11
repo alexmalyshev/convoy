7ac7639f8b0ed45a9f2d44d2097b472a9fdb9f7d
Alexander Malyshev <lex.malyshev@gmail.com>
2012-02-11 04:31:42 -0500

    Start of comments, finally!

--- src/deque.c
+++ src/deque.c
@@ -9,31 +9,38 @@
  *  @author Alexander Malyshev
  */
 
-#include <assert.h>
-#include <stdlib.h>
 #include "deque.h"
 
+#include <assert.h>
+#include <stdlib.h>
+
 static dnode *init_node(void *elem);
+
 
 void deque_init(deque *deq) {
     assert(deq != NULL);
 
+    /* set the deque's fields to be empty */
     deq->front = NULL;
     deq->back = NULL;
     deq->len = 0;
 }
 
 void deque_clear(deque *deq) {
-    dnode *dead, *node;
+    dnode *dead;
+    dnode *node;
 
     assert(deq != NULL);
 
+    /* loop through all nodes in the deque and free them */
     node = deq->front;
     while (node != NULL) {
         dead = node;
         node = node->next;
         free(dead);
     }
+
+    /* set the deque's fields to be empty */
     deq->len = 0;
     deq->front = NULL;
     deq->back = NULL;
@@ -45,15 +52,22 @@ void deque_insertb(deque *deq, void *elem) {
     assert(deq != NULL);
     assert(elem != NULL);
 
+    /* make our new node */
     new = init_node(elem);
 
+    /* set our new node as the new back node of the deque */
     new->prev = deq->back;
     new->next = NULL;
     deq->back = new;
-    if (deq->front == NULL)
-        deq->front = new;
-    else
+
+    /* if the deque is non-empty, set the old back node's next pointer to our
+     * new node. otherwise, set the deque's front pointer to our new node */
+    if (deq->front != NULL)
         new->prev->next = new;
+    else
+        deq->front = new;
+
+    /* update length */
     ++(deq->len);
 }
 
@@ -63,31 +77,48 @@ void deque_insertf(deque *deq, void *elem) {
     assert(deq != NULL);
     assert(elem != NULL);
 
+    /* make our new node */
     new = init_node(elem);
 
+    /* set our new node as the new front node of the deque */
     new->next = deq->front;
     new->prev = NULL;
     deq->front = new;
-    if (deq->back == NULL)
-        deq->back = new;
-    else
+
+    /* if the deque is non-empty, set the old front node's prev pointer to our
+     * new node. otherwise, set the deque's back pointer to our new node */
+    if (deq->back != NULL)
         new->next->prev = new;
+    else
+        deq->back = new;
+
+    /* update length */
     ++(deq->len);
 }
 
 void *deque_peekb(deque *deq) {
     assert(deq != NULL);
 
-    if (deq->back == NULL)
+    /* "good" empty deque means we return NULL */
+    if (deq->front == NULL || deq->back == NULL || deq->len == 0) {
+        assert(deq->front == NULL && deq->back == NULL && deq->len == 0);
         return NULL;
+    }
+
+    /* otherwise return the back element */
     return deq->back->elem;
 }
 
 void *deque_peekf(deque *deq) {
     assert(deq != NULL);
 
-    if (deq->front == NULL)
+    /* "good" empty deque means we return NULL */
+    if (deq->front == NULL || deq->back == NULL || deq->len == 0) {
+        assert(deq->front == NULL && deq->back == NULL && deq->len == 0);
         return NULL;
+    }
+
+    /* otherwise return the front element */
     return deq->front->elem;
 }
 
@@ -97,18 +128,32 @@ void *deque_removeb(deque *deq) {
 
     assert(deq != NULL);
 
-    if (deq->back == NULL)
+    /* "good" empty deque means we return NULL */
+    if (deq->front == NULL || deq->back == NULL || deq->len == 0) {
+        assert(deq->front == NULL && deq->back == NULL && deq->len == 0);
         return NULL;
+    }
 
+    /* pull out the back node */
     dead = deq->back;
-    elem = dead->elem;
     deq->back = dead->prev;
+
+    /* if the deque only had one element, then make sure to set its front
+     * node to NULL. otherwise set its new back node's next pointer to NULL */
     if (deq->back == NULL)
         deq->front = NULL;
     else
         deq->back->next = NULL;
+
+    /* get the back element */
+    elem = dead->elem;
+
+    /* free the back node */
     free(dead);
+
+    /* update length */
     --(deq->len);
+
     return elem;
 }
 
@@ -118,24 +163,46 @@ void *deque_removef(deque *deq) {
 
     assert(deq != NULL);
 
-    if (deq->front == NULL)
+    /* "good" empty deque means we return NULL */
+    if (deq->front == NULL || deq->back == NULL || deq->len == 0) {
+        assert(deq->front == NULL && deq->back == NULL && deq->len == 0);
         return NULL;
+    }
 
+    /* pull out the front node */
     dead = deq->front;
-    elem = dead->elem;
     deq->front = dead->next;
+
+    /* if the deque only had one element, then make sure to set its back
+     * node to NULL. otherwise set its new front node's prev pointer to NULL */
     if (deq->front == NULL)
         deq->back = NULL;
     else
         deq->front->prev = NULL;
+
+    /* get the front element */
+    elem = dead->elem;
+
+    /* free the front node */
     free(dead);
+
+    /* update length */
     --(deq->len);
+
     return elem;
 }
 
 static dnode *init_node(void *elem) {
+    assert(elem != NULL);
+
     dnode *node = malloc(sizeof(dnode));
     assert(node != NULL);
+
+    /* initialize the node's element to 'elem' and its prev
+     * and next pointers to NULL */
     node->elem = elem;
+    node->prev = NULL;
+    node->next = NULL;
+
     return node;
 }
--- src/queue.c
+++ src/queue.c
@@ -8,31 +8,38 @@
  *  @author Alexander Malyshev
  */
 
-#include <assert.h>
-#include <stdlib.h>
 #include "queue.h"
 
+#include <assert.h>
+#include <stdlib.h>
+
 static qnode *init_node(void *elem);
+
 
 void queue_init(queue *qu) {
     assert(qu != NULL);
 
+    /* set the queue's fields to be empty */
     qu->front = NULL;
     qu->back = NULL;
     qu->len = 0;
 }
 
 void queue_clear(queue *qu) {
-    qnode *node, *dead;
+    qnode *node;
+    qnode *dead;
 
     assert(qu != NULL);
 
+    /* loop through all nodes in the queue and free them */
     node = qu->front;
     while (node != NULL) {
         dead = node;
         node = node->next;
         free(dead);
     }
+
+    /* set the queue's fields to be empty */
     qu->front = NULL;
     qu->back = NULL;
     qu->len = 0;
@@ -44,16 +51,30 @@ void *queue_dequeue(queue *qu) {
 
     assert(qu != NULL);
 
-    if (qu->front == NULL)
+    /* "good" empty queue means we return NULL */
+    if (qu->front == NULL || qu->back == NULL || qu->len == 0) {
+        assert(qu->front == NULL && qu->back == NULL && qu->len == 0);
         return NULL;
+    }
 
+    /* pull out the front node */
     dead = qu->front;
     qu->front = dead->next;
+
+    /* if the queue only had one element, then make
+     * sure to set its back node to NULL */
     if (qu->front == NULL)
         qu->back = NULL;
+
+    /* get the front element */
     elem = dead->elem;
+
+    /* free the front node */
     free(dead);
+
+    /* update length */
     --(qu->len);
+
     return elem;
 }
 
@@ -63,30 +84,45 @@ void queue_enqueue(queue *qu, void *elem) {
     assert(qu != NULL);
     assert(elem != NULL);
 
+    /* make our new node */
     new = init_node(elem);
 
+    /* if the queue is non-empty, set its back node's next pointer to be our
+     * new node. otherwise, set the queue's front pointer to our new node */
     if (qu->front != NULL)
         qu->back->next = new;
     else
         qu->front = new;
+
+    /* our new node is the new back node of the queue */
     qu->back = new;
+
+    /* update length */
     ++(qu->len);
 }
 
 void *queue_peek(queue *qu) {
     assert(qu != NULL);
 
-    if (qu->front == NULL)
+    /* "good" empty queue means we return NULL */
+    if (qu->front == NULL || qu->back == NULL || qu->len == 0) {
+        assert(qu->front == NULL && qu->back == NULL && qu->len == 0);
         return NULL;
+    }
 
+    /* otherwise return the front element */
     return qu->front->elem;
 }
 
 static qnode *init_node(void *elem) {
+    assert(elem != NULL);
+
     qnode *node = malloc(sizeof(qnode));
     assert(node != NULL);
 
+    /* initialize the node's element to 'elem' and its next pointer to NULL */
     node->elem = elem;
     node->next = NULL;
+
     return node;
 }
